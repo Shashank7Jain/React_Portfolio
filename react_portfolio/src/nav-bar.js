68cbd41174ffd93b3130c53011265bd8461561f1
sjai6788 <sjai6788@uni.sydney.edu.au>
2020-08-04 12:21:50 +1000

    Nav-bar-outline

---
+++ react_portfolio/src/nav-bar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+//import logo from 'src/icon.png';
+
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+    <div>
+      <Navbar bg="#fcfeff" expand="lg">
+        <Navbar.Brand href="#home">
+      <img
+        alt="Shashank Jain"
+        src="icon.png"
+        width= "30"
+        height="30"
+      />
+      </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto" >
+            <Nav.Link href="#portfolio">PORTFOLIO</Nav.Link>
+            <Nav.Link href="#about">ABOUT</Nav.Link>
+            <Nav.Link href="#resume">RESUME</Nav.Link>
+            <Nav.Link href="#blog">BLOG</Nav.Link>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+    );
+  }
+}
+
+export default NavBar;
