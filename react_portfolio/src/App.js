c5f837dfbeb14489eff716ff525088ce986b383f
sjai6788 <sjai6788@uni.sydney.edu.au>
2020-08-05 13:17:07 +1000

    footer added

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './nav-bar';
+import Footer from './Components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      
+      <Footer/>
     </div>
   );
 }
---
+++ react_portfolio/src/Components/Footer/footer.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styles from 'styled-components';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import {SocialMediaIconsReact} from 'social-media-icons-react';
+
+const DivStyles = styles.div`
+    height :500px;
+    background: black;`
+const DivInner = styles.div`
+    padding:10px`
+const H1Styles = styles.h1`
+    color :white;
+    padding-bottom:50px;
+    padding-top:50px`
+const NavColor = styles.nav`
+    color: white !important ;`
+const DeclareP = styles.p`
+    color: gray;
+    margin-top:50px;
+    margin-bottom:50px;
+    `
+const Footer = () => {
+    return ( 
+        <DivStyles>
+            <DivInner>
+            <H1Styles>Let me build the website you need!</H1Styles>
+            <div>
+                <Navbar bg="black" variant="dark">
+                <Nav className="mx-auto" >
+                    <Nav.Link  href="#portfolio">PORTFOLIO</Nav.Link>
+                    <Nav.Link href="#about">ABOUT</Nav.Link>
+                    <Nav.Link href="#resume">RESUME</Nav.Link>
+                    <Nav.Link href="#blog">BLOG</Nav.Link>
+                    <Nav.Link href="#contact">CONTACT</Nav.Link>
+                </Nav>
+                </Navbar>
+            </div>
+            <div>
+                <DeclareP>2020 Shashank Jain. Developer of this website.</DeclareP>
+            </div>
+            <div>
+            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="0" borderStyle="solid" icon="linkedin" iconColor="rgba(11,10,10,1)" backgroundColor="rgba(250,250,255,1)" iconSize="3" roundness="41%" url="https://www.linkedin.com/in/shashank-jain-b47a2b123/" size="25" />
+            &nbsp;&nbsp;
+            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="0" borderStyle="solid" icon="facebook" iconColor="rgba(11,10,10,1)" backgroundColor="rgba(250,250,255,1)" iconSize="3" roundness="41%" url="https://www.facebook.com/shashank.jain.15" size="25" /> 
+            &nbsp;&nbsp;
+            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="0" borderStyle="solid" icon="instagram" iconColor="rgba(11,10,10,1)" backgroundColor="rgba(250,250,255,1)" iconSize="3" roundness="41%" url="https://www.instagram.com/shashank7jain/" size="25" />
+            &nbsp;&nbsp;
+            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="0" borderStyle="solid" icon="github" iconColor="rgba(11,10,10,1)" backgroundColor="rgba(250,250,255,1)" iconSize="3" roundness="41%" url="https://www.insthttps://github.com/Shashank7Jainagram.com/shashank7jain/" size="25" />
+            &nbsp;&nbsp;
+            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="0" borderStyle="solid" icon="mail" iconColor="rgba(11,10,10,1)" backgroundColor="rgba(250,250,255,1)" iconSize="3" roundness="41%" url="mailto:sjai6788@uni.sydney.edu.au" size="25" />
+            </div>
+            </DivInner>
+
+        </DivStyles>
+     );
+}
+ 
+export default Footer;
