1a88ba9a9672551f57531286dcd09a7406f8fec1
sjai6788 <sjai6788@uni.sydney.edu.au>
2020-08-14 17:04:19 +1000

    Location pointed

--- react_portfolio/src/Components/Contact/location.js
+++ react_portfolio/src/Components/Contact/location.js
@@ -6,12 +6,12 @@ import {Map,Marker,Popup,TileLayer} from "react-leaflet";
 
 const Location = (props) => {
     return ( <div>
-        <Map center={[-33.792382, 150.945232]} zoom={20}>
+        <Map center={[-33.792382, 150.945232]} zoom={15}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505,-0.09]}>
+        <Marker position={[-33.792382, 150.945232]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
