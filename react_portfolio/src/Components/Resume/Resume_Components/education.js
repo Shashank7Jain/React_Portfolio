a307296771ba1cf72b7b8cb80699c7706d36d045
sjai6788 <sjai6788@uni.sydney.edu.au>
2020-08-15 20:51:18 +1000

    minor modification

--- react_portfolio/src/Components/Resume/Resume_Components/education.js
+++ react_portfolio/src/Components/Resume/Resume_Components/education.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+//import resume_data from 'resume_data';
 
 const Education = (props) => {
+   // console.log(resume_data);
     return ( <div>
         <h1>Education! HERE!</h1>
     </div> );
